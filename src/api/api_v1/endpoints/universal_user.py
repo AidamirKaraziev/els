948394740d3b6d2a185445bf3d40cfd4ce8e4ccc
ajdamirkaraziev <a.karaziev@rusrobots.ru>
2025-10-06 23:10:07 +0300

    исправил universal-user/sort-by-role/{role_id}

--- src/api/api_v1/endpoints/universal_user.py
+++ src/api/api_v1/endpoints/universal_user.py
@@ -15,7 +15,11 @@ from src.crud.users.crud_universal_user import crud_universal_users
 from src.crud.crud_company import crud_company
 from src.crud.crud_role import crud_role
 from src.getters.universal_user import get_universal_user
-from src.schemas.universal_user import UniversalUserEntrance, UniversalUserGet, UniversalUserUpdate
+from src.schemas.universal_user import (
+    UniversalUserEntrance,
+    UniversalUserGet,
+    UniversalUserUpdate,
+)
 from src.schemas.token import TokenBase
 
 
@@ -29,120 +33,147 @@ router = APIRouter()
 
 # SIGN-IN
 # Вход по почте и паролю
-@router.post(path='/cp/sign-in/',
-             response_model=SingleEntityResponse[TokenBase],
-             name='Войти в админ панель',
-             description='Войти в админ панель',
-             tags=['Вход / Админ панель'])
+@router.post(
+    path="/cp/sign-in/",
+    response_model=SingleEntityResponse[TokenBase],
+    name="Войти в админ панель",
+    description="Войти в админ панель",
+    tags=["Вход / Админ панель"],
+)
 def entrance(
     entrance_data: UniversalUserEntrance,
     session: Session = Depends(deps.get_db),
 ):
-    db_obj = crud_universal_users.entrance_universal_user(db=session, entrance_data=entrance_data)
+    db_obj = crud_universal_users.entrance_universal_user(
+        db=session, entrance_data=entrance_data
+    )
     token = create_token_universal_user(subject=db_obj.id)
     return SingleEntityResponse(data=TokenBase(token=token))
 
 
 # GET-MULTY
-@router.get('/cp/all-users/',
-            response_model=ListOfEntityResponse,
-            name='Список пользователей',
-            description='Получение списка всех пользователей',
-            tags=['Админ панель / Пользователь']
-            )
+@router.get(
+    "/cp/all-users/",
+    response_model=ListOfEntityResponse,
+    name="Список пользователей",
+    description="Получение списка всех пользователей",
+    tags=["Админ панель / Пользователь"],
+)
 def get_data(
-        request: Request,
-        session=Depends(deps.get_db),
-        page: int = Query(1, title="Номер страницы")
+    request: Request,
+    session=Depends(deps.get_db),
+    page: int = Query(1, title="Номер страницы"),
 ):
     logging.info(crud_universal_users.get_multi(db=session, page=None))
 
     data, paginator = crud_universal_users.get_multi(db=session, page=page)
 
-    return ListOfEntityResponse(data=[get_universal_user(datum, request=request) for datum in data],
-                                meta=Meta(paginator=paginator))
+    return ListOfEntityResponse(
+        data=[get_universal_user(datum, request=request) for datum in data],
+        meta=Meta(paginator=paginator),
+    )
 
 
 # GET user by role_id
-@router.get('/universal-user/sort-by-role/{role_id}/',
-            response_model=ListOfEntityResponse,
-            name='get_users_by_role_id',
-            description='Получение пользователей по роли',
-            tags=['Админ панель / Пользователь']
-            )
+@router.get(
+    "/universal-user/sort-by-role/{role_id}/",
+    response_model=ListOfEntityResponse,
+    name="get_users_by_role_id",
+    description="Получение пользователей по роли",
+    tags=["Админ панель / Пользователь"],
+)
 def get_users_by_role_id(
-        request: Request,
-        role_id: int = Path(..., title='ID модели техники'),
-        # current_user=Depends(deps.get_current_universal_user_by_bearer),
-        session=Depends(deps.get_db),
-        page: int = Query(1, title="Номер страницы")
+    request: Request,
+    role_id: int = Path(..., title="ID модели техники"),
+    # current_user=Depends(deps.get_current_universal_user_by_bearer),
+    session=Depends(deps.get_db),
+    page: Optional[int] = Query(None, title="Номер страницы"),
 ):
     obj, code, indexes = crud_role.get_role_by_id(db=session, role_id=role_id)
     get_raise(code=code)
-    data, paginator = crud_universal_users.get_user_by_role_id(db=session, page=page, role_id=role_id)
+    data, paginator = crud_universal_users.get_user_by_role_id(
+        db=session, page=page, role_id=role_id
+    )
 
-    return ListOfEntityResponse(data=[get_universal_user(datum, request) for datum in data],
-                                meta=Meta(paginator=paginator))
+    return ListOfEntityResponse(
+        data=[get_universal_user(datum, request) for datum in data],
+        meta=Meta(paginator=paginator),
+    )
 
 
 # GET-MULTY EMPLOYEE
-@router.get('/cp/all-employee/',
-            response_model=ListOfEntityResponse,
-            name='Список сотрудников',
-            description='Получение списка всех сотрудников',
-            tags=['Админ панель / Пользователь']
-            )
+@router.get(
+    "/cp/all-employee/",
+    response_model=ListOfEntityResponse,
+    name="Список сотрудников",
+    description="Получение списка всех сотрудников",
+    tags=["Админ панель / Пользователь"],
+)
 def get_data(
-        request: Request,
-        session=Depends(deps.get_db),
-        page: int = Query(1, title="Номер страницы")
+    request: Request,
+    session=Depends(deps.get_db),
+    page: int = Query(1, title="Номер страницы"),
 ):
     logging.info(crud_universal_users.get_multi_employee(db=session, page=None))
 
     data, paginator = crud_universal_users.get_multi_employee(db=session, page=page)
 
-    return ListOfEntityResponse(data=[get_universal_user(datum, request=request) for datum in data],
-                                meta=Meta(paginator=paginator))
+    return ListOfEntityResponse(
+        data=[get_universal_user(datum, request=request) for datum in data],
+        meta=Meta(paginator=paginator),
+    )
 
 
 # GET CLIENT OF COMPANY
-@router.get('/cp/client/{company_id}/',
-            response_model=ListOfEntityResponse,
-            name='Список клиентов определенной компании',
-            description='Список клиентов определенной компании',
-            tags=['Админ панель / Пользователь']
-            )
+@router.get(
+    "/cp/client/{company_id}/",
+    response_model=ListOfEntityResponse,
+    name="Список клиентов определенной компании",
+    description="Список клиентов определенной компании",
+    tags=["Админ панель / Пользователь"],
+)
 def get_data(
-        request: Request,
-        session=Depends(deps.get_db),
-        company_id: int = Path(..., title='ID user'),
-        current_user=Depends(deps.get_current_universal_user_by_bearer),
-        page: int = Query(1, title="Номер страницы")
+    request: Request,
+    session=Depends(deps.get_db),
+    company_id: int = Path(..., title="ID user"),
+    current_user=Depends(deps.get_current_universal_user_by_bearer),
+    page: int = Query(1, title="Номер страницы"),
 ):
     # проверка компании
-    comp, code, indexes = crud_company.get_company_by_id(db=session, company_id=company_id)
+    comp, code, indexes = crud_company.get_company_by_id(
+        db=session, company_id=company_id
+    )
     get_raise(code=code)
 
-    logging.info(crud_universal_users.get_multi_client_by_company(db=session, company_id=company_id, page=None))
+    logging.info(
+        crud_universal_users.get_multi_client_by_company(
+            db=session, company_id=company_id, page=None
+        )
+    )
 
-    data, paginator = crud_universal_users.get_multi_client_by_company(db=session, page=page, company_id=company_id)
+    data, paginator = crud_universal_users.get_multi_client_by_company(
+        db=session, page=page, company_id=company_id
+    )
 
-    return ListOfEntityResponse(data=[get_universal_user(datum, request=request) for datum in data],
-                                meta=Meta(paginator=paginator))
+    return ListOfEntityResponse(
+        data=[get_universal_user(datum, request=request) for datum in data],
+        meta=Meta(paginator=paginator),
+    )
 
 
 # GET CLIENT
-@router.get('/cp/all-client/',
-            response_model=ListOfEntityResponse,
-            name='Список клиентов ',
-            description='Список клиентов',
-            tags=['Админ панель / Пользователь']
-            )
+@router.get(
+    "/cp/all-client/",
+    response_model=ListOfEntityResponse,
+    name="Список клиентов ",
+    description="Список клиентов",
+    tags=["Админ панель / Пользователь"],
+)
 def get_data(
-        request: Request,
-        session=Depends(deps.get_db),
-        current_user=Depends(deps.get_current_universal_user_by_bearer),
-        page: int = Query(1, title="Номер страницы")
+    request: Request,
+    session=Depends(deps.get_db),
+    current_user=Depends(deps.get_current_universal_user_by_bearer),
+    page: int = Query(1, title="Номер страницы"),
 ):
     # проверка компании
 
@@ -150,36 +181,42 @@ def get_data(
 
     data, paginator = crud_universal_users.get_multi_clients(db=session, page=page)
 
-    return ListOfEntityResponse(data=[get_universal_user(datum, request=request) for datum in data],
-                                meta=Meta(paginator=paginator))
+    return ListOfEntityResponse(
+        data=[get_universal_user(datum, request=request) for datum in data],
+        meta=Meta(paginator=paginator),
+    )
 
 
 # GET
-@router.get('/cp/universal-user/me/',
-            response_model=SingleEntityResponse[UniversalUserGet],
-            name='Получить данные профиля ',
-            description='Получение всех  данных профиля, по токену',
-            tags=['Админ панель / Пользователь']
-            )
+@router.get(
+    "/cp/universal-user/me/",
+    response_model=SingleEntityResponse[UniversalUserGet],
+    name="Получить данные профиля ",
+    description="Получение всех  данных профиля, по токену",
+    tags=["Админ панель / Пользователь"],
+)
 def get_data(
-        request: Request,
-        current_universal_user=Depends(deps.get_current_universal_user_by_bearer),
+    request: Request,
+    current_universal_user=Depends(deps.get_current_universal_user_by_bearer),
 ):
-    return SingleEntityResponse(data=get_universal_user(current_universal_user, request=request))
+    return SingleEntityResponse(
+        data=get_universal_user(current_universal_user, request=request)
+    )
 
 
 # GET BY ID
-@router.get('/cp/universal-user/{user_id}/',
-            response_model=SingleEntityResponse[UniversalUserGet],
-            name='Получить данные профиля по id ',
-            description='Получение данных профиля по id',
-            tags=['Админ панель / Пользователь']
-            )
+@router.get(
+    "/cp/universal-user/{user_id}/",
+    response_model=SingleEntityResponse[UniversalUserGet],
+    name="Получить данные профиля по id ",
+    description="Получение данных профиля по id",
+    tags=["Админ панель / Пользователь"],
+)
 def get_data(
-        request: Request,
-        session=Depends(deps.get_db),
-        user_id: int = Path(..., title='ID user'),
-        current_universal_user=Depends(deps.get_current_universal_user_by_bearer),
+    request: Request,
+    session=Depends(deps.get_db),
+    user_id: int = Path(..., title="ID user"),
+    current_universal_user=Depends(deps.get_current_universal_user_by_bearer),
 ):
     user = crud_universal_users.get(db=session, id=user_id)
     if user is None:
@@ -187,109 +224,132 @@ def get_data(
             message="Нет такого пользователя!",
             num=105,
             description="Нет пользователя с таким id!",
-            path="$.body"
+            path="$.body",
         )
     return SingleEntityResponse(data=get_universal_user(user, request=request))
 
 
 # UPDATE SELF
-@router.put('/cp/universal-user/me/',
-            response_model=SingleEntityResponse[UniversalUserGet],
-            name='Изменить пользователя',
-            description='Изменить данные текущего пользователя',
-            tags=['Админ панель / Пользователь']
-            )
+@router.put(
+    "/cp/universal-user/me/",
+    response_model=SingleEntityResponse[UniversalUserGet],
+    name="Изменить пользователя",
+    description="Изменить данные текущего пользователя",
+    tags=["Админ панель / Пользователь"],
+)
 def update_user(
-        request: Request,
-        new_data: UniversalUserUpdate,
-        current_user=Depends(deps.get_current_universal_user_by_bearer),
-        session=Depends(deps.get_db),
+    request: Request,
+    new_data: UniversalUserUpdate,
+    current_user=Depends(deps.get_current_universal_user_by_bearer),
+    session=Depends(deps.get_db),
 ):
     role_list = [current_user.role_id]
     changeable_list = role_list
-    obj, code, indexes = crud_universal_users.updating_user(db=session,
-                                                            current_user=current_user,
-                                                            user_id=current_user.id,
-                                                            new_data=new_data,
-                                                            role_list=role_list,
-                                                            changeable_list=changeable_list)
+    obj, code, indexes = crud_universal_users.updating_user(
+        db=session,
+        current_user=current_user,
+        user_id=current_user.id,
+        new_data=new_data,
+        role_list=role_list,
+        changeable_list=changeable_list,
+    )
 
     get_raise(code=code)
     return SingleEntityResponse(data=get_universal_user(obj, request=request))
 
 
 # UPDATE photo
-@router.put("/cp/universal-user/me/photo/",
-            response_model=SingleEntityResponse,
-            name='Изменить фото',
-            description='Изменить фото для пользователя, если отправить пусто поле информация сбросится',
-            tags=['Админ панель / Пользователь'],
-            )
+@router.put(
+    "/cp/universal-user/me/photo/",
+    response_model=SingleEntityResponse,
+    name="Изменить фото",
+    description="Изменить фото для пользователя, если отправить пусто поле информация сбросится",
+    tags=["Админ панель / Пользователь"],
+)
 def create_upload_file(
-        request: Request,
-        file: Optional[UploadFile] = File(None),
-        current_user=Depends(deps.get_current_universal_user_by_bearer),
-        session=Depends(deps.get_db),
-        ):
+    request: Request,
+    file: Optional[UploadFile] = File(None),
+    current_user=Depends(deps.get_current_universal_user_by_bearer),
+    session=Depends(deps.get_db),
+):
     save_path = crud_universal_users.adding_file(
-        db=session, file=file, path_model=PATH_MODEL, path_type=PATH_TYPE_PHOTO, db_obj=current_user)
+        db=session,
+        file=file,
+        path_model=PATH_MODEL,
+        path_type=PATH_TYPE_PHOTO,
+        db_obj=current_user,
+    )
     if not save_path:
-        raise UnfoundEntity(message="Не отправлен загружаемый файл",
-                            num=2,
-                            description="Попробуйте загрузить файл еще раз",
-                            path="$.body",
-                            )
+        raise UnfoundEntity(
+            message="Не отправлен загружаемый файл",
+            num=2,
+            description="Попробуйте загрузить файл еще раз",
+            path="$.body",
+        )
     return SingleEntityResponse(data=get_universal_user(current_user, request=request))
 
 
 # UPDATE identity-card
-@router.put("/cp/universal-user/me/identity-card/",
-            response_model=SingleEntityResponse,
-            name='Изменить удостоверение',
-            description='Изменить удостоверение для пользователя, если отправить пусто поле информация сбросится',
-            tags=['Админ панель / Пользователь'],
-            )
+@router.put(
+    "/cp/universal-user/me/identity-card/",
+    response_model=SingleEntityResponse,
+    name="Изменить удостоверение",
+    description="Изменить удостоверение для пользователя, если отправить пусто поле информация сбросится",
+    tags=["Админ панель / Пользователь"],
+)
 def create_upload_file(
-        request: Request,
-        file: Optional[UploadFile] = File(None),
-        current_user=Depends(deps.get_current_universal_user_by_bearer),
-        session=Depends(deps.get_db),
-        ):
+    request: Request,
+    file: Optional[UploadFile] = File(None),
+    current_user=Depends(deps.get_current_universal_user_by_bearer),
+    session=Depends(deps.get_db),
+):
     save_path = crud_universal_users.adding_file(
-        db=session, file=file, path_model=PATH_MODEL, path_type=PATH_TYPE_IDENTITY_CARD, db_obj=current_user)
+        db=session,
+        file=file,
+        path_model=PATH_MODEL,
+        path_type=PATH_TYPE_IDENTITY_CARD,
+        db_obj=current_user,
+    )
     if not save_path:
-        raise UnfoundEntity(message="Не отправлен загружаемый файл",
-                            num=2,
-                            description="Попробуйте загрузить файл еще раз",
-                            path="$.body",
-                            )
+        raise UnfoundEntity(
+            message="Не отправлен загружаемый файл",
+            num=2,
+            description="Попробуйте загрузить файл еще раз",
+            path="$.body",
+        )
     return SingleEntityResponse(data=get_universal_user(current_user, request=request))
 
 
 # UPDATE qualification_file
-@router.put("/cp/universal-user/me/qualification-file/",
-            response_model=SingleEntityResponse,
-            name='Изменить ЦОК',
-            description='Изменить ЦОК для пользователя, если отправить пусто поле информация сбросится',
-            tags=['Админ панель / Пользователь'],
-            )
+@router.put(
+    "/cp/universal-user/me/qualification-file/",
+    response_model=SingleEntityResponse,
+    name="Изменить ЦОК",
+    description="Изменить ЦОК для пользователя, если отправить пусто поле информация сбросится",
+    tags=["Админ панель / Пользователь"],
+)
 def create_upload_file(
-        request: Request,
-        file: Optional[UploadFile] = File(None),
-        current_user=Depends(deps.get_current_universal_user_by_bearer),
-        session=Depends(deps.get_db),
-        ):
+    request: Request,
+    file: Optional[UploadFile] = File(None),
+    current_user=Depends(deps.get_current_universal_user_by_bearer),
+    session=Depends(deps.get_db),
+):
     save_path = crud_universal_users.adding_file(
-        db=session, file=file, path_model=PATH_MODEL, path_type=PATH_TYPE_QUALIFICATION, db_obj=current_user)
+        db=session,
+        file=file,
+        path_model=PATH_MODEL,
+        path_type=PATH_TYPE_QUALIFICATION,
+        db_obj=current_user,
+    )
     if not save_path:
-        raise UnfoundEntity(message="Не отправлен загружаемый файл",
-                            num=2,
-                            description="Попробуйте загрузить файл еще раз",
-                            path="$.body",
-                            )
+        raise UnfoundEntity(
+            message="Не отправлен загружаемый файл",
+            num=2,
+            description="Попробуйте загрузить файл еще раз",
+            path="$.body",
+        )
     return SingleEntityResponse(data=get_universal_user(current_user, request=request))
 
 
 if __name__ == "__main__":
-    logging.info('Running...')
-
+    logging.info("Running...")
--- src/crud/users/crud_universal_user.py
+++ src/crud/users/crud_universal_user.py
@@ -15,30 +15,36 @@ from src.models import UniversalUser, Location, Division, WorkingSpecialty
 
 from src.schemas.foreman import ForemanCreate
 from src.schemas.universal_user import (
-    UniversalUserCreate, UniversalUserUpdate, UniversalUserEntrance)
+    UniversalUserCreate,
+    UniversalUserUpdate,
+    UniversalUserEntrance,
+)
 
 ADMIN_LIST = [ADMIN]
 
 
-class CrudUniversalUser(CRUDBaseUser[
-                            UniversalUser,
-                            UniversalUserCreate,
-                            UniversalUserUpdate]):
+class CrudUniversalUser(
+    CRUDBaseUser[UniversalUser, UniversalUserCreate, UniversalUserUpdate]
+):
 
     def create_foreman(
-            self,  db: Session, *, current_user: UniversalUser,
-            new_data: ForemanCreate):
+        self, db: Session, *, current_user: UniversalUser, new_data: ForemanCreate
+    ):
         # проверить есть ли такой current_user
-        admin = db.query(UniversalUser).filter(
-            UniversalUser.id == current_user.id).first()
+        admin = (
+            db.query(UniversalUser).filter(UniversalUser.id == current_user.id).first()
+        )
         if admin is None:
             return None, -1, None
         # проверить должность current_user
         if current_user.role_id != 1:
             return None, -2, None
         # проверка есть ли такой email in db
-        email = db.query(UniversalUser).filter(
-            UniversalUser.email == new_data.email).first()
+        email = (
+            db.query(UniversalUser)
+            .filter(UniversalUser.email == new_data.email)
+            .first()
+        )
         if email is not None:
             return None, -3, None  # have email in db
         # проверять хеш пароль
@@ -50,8 +56,7 @@ class CrudUniversalUser(CRUDBaseUser[
             new_data.birthday = date_from_timestamp(new_data.birthday)
 
         if new_data.location_id is not None:
-            loc = db.query(Location).filter(
-                Location.id == new_data.location_id).first()
+            loc = db.query(Location).filter(Location.id == new_data.location_id).first()
             if loc is None:
                 return None, -4, None  # нет города
 
@@ -59,64 +64,64 @@ class CrudUniversalUser(CRUDBaseUser[
             return None, -5, None
 
         if new_data.working_specialty_id is not None:
-            spec = db.query(WorkingSpecialty).filter(
-                WorkingSpecialty.id == new_data.working_specialty_id).first()
+            spec = (
+                db.query(WorkingSpecialty)
+                .filter(WorkingSpecialty.id == new_data.working_specialty_id)
+                .first()
+            )
             if spec is None:
                 return None, -6, None
         # Проверить участок
         if new_data.division_id is not None:
-            div = db.query(Division).filter(
-                Division.id == new_data.division_id).first()
+            div = db.query(Division).filter(Division.id == new_data.division_id).first()
             if div is None:
                 return None, -7, None
         db_obj = super().create(db=db, obj_in=new_data)
         return db_obj, 0, None
 
     def entrance_universal_user(
-            self, *,
-            db: Session,
-            entrance_data: UniversalUserEntrance
+        self, *, db: Session, entrance_data: UniversalUserEntrance
     ):
-        current_user = db.query(
-            UniversalUser).filter(
-            UniversalUser.email == entrance_data.email).first()
+        current_user = (
+            db.query(UniversalUser)
+            .filter(UniversalUser.email == entrance_data.email)
+            .first()
+        )
         if current_user is None or not verify_password(
-                plain_password=entrance_data.password,
-                hashed_password=current_user.password):
+            plain_password=entrance_data.password, hashed_password=current_user.password
+        ):
             raise UnprocessableEntity(
                 message="Неверный логин или пароль",
                 num=1,
                 description="Неверный логи или пароль",
-                path="$.body"
+                path="$.body",
             )
         if current_user.is_actual is False:
             raise UnprocessableEntity(
                 message="Вам отказано в доступе",
                 num=1,
                 description="Администратор ограничил вам доступ",
-                path="$.body"
+                path="$.body",
             )
 
         return current_user
 
     def get_by_email(self, db: Session, *, email: str):
-        return db.query(UniversalUser).filter(
-            UniversalUser.email == email).first()
+        return db.query(UniversalUser).filter(UniversalUser.email == email).first()
 
     def get_user_by_id(self, db: Session, *, user_id: int):
-        user = db.query(UniversalUser).filter(
-            UniversalUser.id == user_id).first()
+        user = db.query(UniversalUser).filter(UniversalUser.id == user_id).first()
         if user is None:
             return None, -130, None
         return user, 0, None
 
     def get_user_by_role_id(
-            self, *, db: Session, role_id: int, page: Optional[int] = None):
+        self, *, db: Session, role_id: int, page: Optional[int] = None
+    ):
         objs = db.query(UniversalUser).filter(UniversalUser.role_id == role_id)
         return pagination.get_page(objs, page)
 
-    def delete_user_by_id(
-            self, *, db: Session, user_id: int, current_user_id: int):
+    def delete_user_by_id(self, *, db: Session, user_id: int, current_user_id: int):
         user, code, indexes = self.get_user_by_id(db=db, user_id=user_id)
         if code != 0:
             return None, code, None
@@ -127,13 +132,22 @@ class CrudUniversalUser(CRUDBaseUser[
 
     def get_clients_by_company_id(self, *, db: Session, company_id: int):
         from src.crud.crud_company import crud_company
+
         # проверка на компанию
-        company, code, indexes = crud_company.get_company_by_id(db=db, company_id=company_id)
+        company, code, indexes = crud_company.get_company_by_id(
+            db=db, company_id=company_id
+        )
         if code != 0:
             return None, code, None
-        clients = db.query(UniversalUser).filter(
-            UniversalUser.company_id == company_id, UniversalUser.role_id == CLIENT_ID
-        ).order_by(UniversalUser.id).all()
+        clients = (
+            db.query(UniversalUser)
+            .filter(
+                UniversalUser.company_id == company_id,
+                UniversalUser.role_id == CLIENT_ID,
+            )
+            .order_by(UniversalUser.id)
+            .all()
+        )
         return clients, 0, None
 
 
